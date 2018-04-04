87b59b0beba926ae8a582e7478f2e18a2649e079
Daniel Doughty <djdoughty@gmail.com>
2018-04-04 18:56:52 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -25,7 +25,7 @@ function doWhile(array) {
    do {
      array.pop() 
      console.log(array)
-   } while (array.length > 0);
+   } while (array.length > 0) && maybeTrue();
 }
 doWhile([1, 3, 5, 7, 9])
 
